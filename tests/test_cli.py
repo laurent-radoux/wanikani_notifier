2145865af61df83eb4319de403805e71e7504ca7
laurent-radoux <50481138+laurent-radoux@users.noreply.github.com>
2021-07-30 19:14:06 +0200

    Add test coverage for process_all

---
+++ tests/test_cli.py
@@ -0,0 +1,73 @@
+from unittest.mock import MagicMock
+
+import pytest
+from pytest_mock import MockerFixture
+
+from wanikani_notifier.cli import process_all, processor, generator
+
+
+@pytest.fixture
+def mocked_wk_client(mocker: MockerFixture) -> MagicMock:
+    return mocker.patch("wanikani_notifier.cli.WaniKaniClient")
+
+
+@pytest.fixture
+def mocked_processor_counter(mocker: MockerFixture):
+    return mocker.Mock()
+
+
+@pytest.fixture
+def mocked_consumer(mocked_processor_counter):
+    @processor
+    def message_consumer(_, message_stream):
+        mocked_processor_counter()
+        yield from message_stream
+
+    return message_consumer()
+
+
+@pytest.fixture
+def mocked_generator(mocked_processor_counter):
+    @generator
+    def message_generator(*unused):
+        mocked_processor_counter()
+        yield "new message"
+
+    return message_generator()
+
+
+@pytest.mark.parametrize("stop_if_empty", [pytest.param(True, id="stop"), pytest.param(False, id="continue")])
+def test_process_all_no_processors(mocked_wk_client, mocked_processor_counter, stop_if_empty):
+    process_all([], "__TOKEN__", stop_if_empty)
+
+    mocked_wk_client.assert_called_once()
+    mocked_processor_counter.assert_not_called()
+
+
+@pytest.mark.parametrize("stop_if_empty", [pytest.param(True, id="stop"), pytest.param(False, id="continue")])
+def test_process_all_with_processors_no_generators(mocked_wk_client,
+                                                   mocked_processor_counter,
+                                                   mocked_consumer,
+                                                   stop_if_empty
+                                                   ):
+    processors = [mocked_consumer, mocked_consumer, mocked_consumer]
+    process_all(processors, "__TOKEN__", stop_if_empty)
+
+    mocked_wk_client.assert_called_once()
+    if stop_if_empty:
+        mocked_processor_counter.assert_called_once()
+    else:
+        assert mocked_processor_counter.call_count == len(processors)
+
+
+@pytest.mark.parametrize("stop_if_empty", [pytest.param(True, id="stop"), pytest.param(False, id="continue")])
+def test_process_all_no_processors_with_generators(mocked_wk_client,
+                                                   mocked_processor_counter,
+                                                   mocked_generator,
+                                                   stop_if_empty
+                                                   ):
+    processors = [mocked_generator, mocked_generator, mocked_generator]
+    process_all(processors, "__TOKEN__", stop_if_empty)
+
+    mocked_wk_client.assert_called_once()
+    assert mocked_processor_counter.call_count == len(processors)
--- wanikani_notifier/__main__.py
+++ wanikani_notifier/__main__.py
@@ -1,4 +1,4 @@
-from wanikani_notifier.cli import cli
+from wanikani_notifier.cli import cli  # pragma: nocover
 
-if __name__ == "__main__":
+if __name__ == "__main__":  # pragma: nocover
     cli()  # pragma: nocover
